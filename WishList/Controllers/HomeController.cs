26701a72ccdcb6470903ea555c004b17eda4771a
Gilles Vanderseypen <gilles.vanderseypen@extern.atrias.be>
2022-11-23 15:21:00 +0100

    Completed module

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return Error();
+        }
+    }
+}
